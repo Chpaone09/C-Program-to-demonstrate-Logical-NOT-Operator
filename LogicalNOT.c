8624595fc3b3354e09ef51946e98d673b9a74f70
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 03:45:52 +0530

    Program to demonstrate Logical NOT ( ! ) Operator in c

---
+++ LogicalNOT.c
@@ -0,0 +1,23 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate Logical NOT ( ! ) Operator in c.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
+
+void main(){
+        bool a = true;
+
+         printf("\n\n\t Original Value of A >> %d", a);
+
+         printf("\n\n\t After applying Logical NOT Operator on A >> %d", !a);
+
+    
+}
